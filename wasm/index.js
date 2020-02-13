ef1b607ad3b88a7bf5315c2acd5b9f83082ee01c
roz3x <meeshivang@gmail.com>
2020-02-14 03:10:41 +0530

    changed the file

--- wasm/index.js
+++ wasm/index.js
@@ -1,5 +1,11 @@
 window.onload = ()=> { 
- 
+  if (!WebAssembly.instantiateStreaming) {
+        // polyfill
+        WebAssembly.instantiateStreaming = async (resp, importObject) => {
+          const source = await (await resp).arrayBuffer();
+          return await WebAssembly.instantiate(source, importObject);
+        };
+      }
       const go = new Go();
 
       let mod, inst
